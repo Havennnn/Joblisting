b2d4132d77707fc0de77a64478e001ba558a834c
Havennnn <latrellmarable00@gmail.com>
2025-04-09 18:13:08 +0800

    Navigation update

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -18,11 +18,10 @@
                         @endif
                     @else
                         <a href="/" class="text-gray-900 hover:text-neksjob-blue px-3 py-2 text-sm font-medium">Home</a>
-                    @endauth 
+                    @endauth
 
-                    <a href="{{ route('employer.JobPost') }}" class="text-gray-900 hover:text-neksjob-blue px-3 py-2 text-sm font-medium">Post jobs</a>
                     <a href="{{ route('jobs.index') }}" class="text-gray-900 hover:text-neksjob-blue px-3 py-2 text-sm font-medium">Find jobs</a>
-                
+
                 </div>
 
                 <div class="flex items-center space-x-4">
