d5d21c548785bb72591786b9ba78e2afed416ec7
Havennnn <latrellmarable00@gmail.com>
2025-04-15 14:59:16 +0800

    style(navbar): update navigation bar design

--- resources/views/components/navigation/auth-buttons.blade.php
+++ resources/views/components/navigation/auth-buttons.blade.php
@@ -1,36 +1,32 @@
 @props([
-    'employerLoginClass' => 'hover:text-neksjob-pink text-sm font-medium transition-colors duration-300 ease-in-out',
-    'applicantLoginClass' => 'hover:text-neksjob-blue text-sm font-medium transition-colors duration-300 ease-in-out',
-    'buttonClass' => 'border border-neksjob-blue rounded-md text-neksjob-blue hover:bg-neksjob-blue hover:text-white px-3 py-1 text-sm font-medium transition-all duration-300 ease-in-out'
+    'employerLoginClass' => 'relative text-md font-light text-nextjob-black group transition-all duration-300 ease-in-out',
+    'applicantLoginClass' => 'relative text-md font-light text-nextjob-black group transition-all duration-300 ease-in-out',
 ])
 
 @auth
-    {{-- If authenticated, nothing to show here as user dropdown will be shown instead --}}
+
 @else
     @if (Route::currentRouteName() == 'applicant.login')
-        <a href="{{ route('employer.login') }}"
-            class="{{ $employerLoginClass }}">
-            Employer Login
+        <a href="{{ route('employer.login') }}" class="{{ $employerLoginClass }}">
+            <span class="block group-hover:text-nextjob-red">Employer Login</span>
+            <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-nextjob-red group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </a>
     @elseif(Route::currentRouteName() == 'employer.login' || Route::currentRouteName() == 'employer.register')
-        <a href="{{ route('applicant.login') }}"
-            class="{{ $applicantLoginClass }}">
-            Jobseeker Login
+        <a href="{{ route('applicant.login') }}" class="{{ $applicantLoginClass }}">
+            <span class="block group-hover:text-nextjob-blue">Jobseeker Login</span>
+            <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-nextjob-blue group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </a>
     @elseif(request()->is('/') || request()->is('jobs') || request()->is('job-details/*') || Route::currentRouteName() == 'content.unavailable')
         <div class="flex justify-center items-center space-x-4">
-            <a href="{{ route('applicant.login') }}"
-                class="{{ $applicantLoginClass }}">
-                Jobseeker Login
+            <a href="{{ route('applicant.login') }}" class="{{ $applicantLoginClass }}">
+                <span class="block group-hover:text-nextjob-blue">Jobseeker Login</span>
+                <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-nextjob-blue group-hover:w-full transition-all duration-300 ease-in-out"></span>
             </a>
             <span class="text-gray-500 text-sm">|</span>
-            <a href="{{ route('employer.login') }}"
-                class="{{ $employerLoginClass }}">
-                Employer Login
+            <a href="{{ route('employer.login') }}" class="{{ $employerLoginClass }}">
+                <span class="block group-hover:text-nextjob-red">Employer Login</span>
+                <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-nextjob-red group-hover:w-full transition-all duration-300 ease-in-out"></span>
             </a>
         </div>
-    @else
-        <a href="{{ route('applicant.login') }}"
-            class="{{ $buttonClass }}">Jobseeker Login</a>
     @endif
 @endauth
--- resources/views/components/navigation/nav-links.blade.php
+++ resources/views/components/navigation/nav-links.blade.php
@@ -1,29 +1,33 @@
 @props([
     'activeTextColor' => 'text-nextjob-blue',
     'defaultTextColor' => 'text-nextjob-black',
-    'hoverState' => 'hover:text-nextjob-blue hover:scale-105',
+    'hoverState' => 'hover:text-nextjob-blue',
     'activeFontWeight' => 'font-semibold',
     'defaultFontWeight' => 'font-light',
-    'transitionDuration' => 'duration-500',
+    'transitionDuration' => 'duration-300',
     'activeClasses' => 'scale-105'
 ])
 
-<div class="flex items-start space-x-4">
-    <a href="/" class="px-3 transition-all {{ $transitionDuration }} ease-in-out">
+<div class="flex items-start space-x-12 ml-4">
+    <a href="/" class="relative group transition-all {{ $transitionDuration }} ease-in-out">
         <span class="{{ request()->is('/') ? $activeTextColor.' '.$activeFontWeight.' '.$activeClasses : $defaultTextColor.' '.$defaultFontWeight }} {{ $hoverState }} text-md transition-all {{ $transitionDuration }} ease-in-out transform inline-block">
             Home
-            @if(request()->is('/'))
-                <span class="block bg-nextjob-blue transition-all {{ $transitionDuration }}"></span>
-            @endif
         </span>
+        @if(request()->is('/'))
+            <span class="absolute bottom-0 left-0 w-full h-0.5 bg-nextjob-blue transition-all {{ $transitionDuration }}"></span>
+        @else
+            <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-nextjob-blue group-hover:w-full transition-all {{ $transitionDuration }} ease-in-out"></span>
+        @endif
     </a>
-    <a href="{{ route('jobs.index') }}" class="px-3 transition-all {{ $transitionDuration }} ease-in-out">
+    <a href="{{ route('jobs.index') }}" class="relative group transition-all {{ $transitionDuration }} ease-in-out">
         <span class="{{ request()->routeIs('jobs.index') ? $activeTextColor.' '.$activeFontWeight.' '.$activeClasses : $defaultTextColor.' '.$defaultFontWeight }} {{ $hoverState }} text-md transition-all {{ $transitionDuration }} ease-in-out transform inline-block">
             Find jobs
-            @if(request()->routeIs('jobs.index'))
-                <span class="block bg-nextjob-blue transition-all {{ $transitionDuration }}"></span>
-            @endif
         </span>
+        @if(request()->routeIs('jobs.index'))
+            <span class="absolute bottom-0 left-0 w-full h-0.5 bg-nextjob-blue transition-all {{ $transitionDuration }}"></span>
+        @else
+            <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-nextjob-blue group-hover:w-full transition-all {{ $transitionDuration }} ease-in-out"></span>
+        @endif
     </a>
 
     {{ $slot ?? '' }}
