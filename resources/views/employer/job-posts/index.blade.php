05b06004b54ad0d7e202116969cf89e9db925fbe
Havennnn <latrellmarable00@gmail.com>
2025-04-24 13:45:04 +0800

    chore: removed legacy alert message

--- resources/views/employer/job-posts/index.blade.php
+++ resources/views/employer/job-posts/index.blade.php
@@ -35,24 +35,6 @@
                 <!-- Profile Completion Alert -->
                 <x-profile-completion-alert :percentage="$completionPercentage ?? 0" userType="employer" />
 
-                <!-- Success Message -->
-                @if(session()->has('success'))
-                <div class="bg-green-50 p-4 mb-6 border-l-2 border-green-500">
-                    <div class="flex">
-                        <div class="flex-shrink-0">
-                            <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                            </svg>
-                        </div>
-                        <div class="ml-3">
-                            <p class="text-sm font-medium text-green-800">
-                                {{ session('success') }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                @endif
-
                 <!-- Job Listings Table -->
                 <div class="bg-white border border-gray-200 shadow-sm">
                     <div class="border-b border-gray-200 p-4">
